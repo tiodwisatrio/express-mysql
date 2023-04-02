39327f494e2a97a4b73b8d7b95fa1150dd592ae8
tiodwisatrio <tiodwisatrio27@gmail.com>
2023-04-02 16:23:50 +0700

    add post method

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const app = express();
 const PORT = 5001;
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+  res.send("Hello Get Method");
+});
+
+app.post("/", (req, res) => {
+  res.send("Hello Post Method");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running http://localhost:${PORT}`);
