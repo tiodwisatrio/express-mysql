becf17e0a49775bbfc3231a896304d0123861203
tiodwisatrio <tiodwisatrio27@gmail.com>
2023-04-05 21:04:19 +0700

    install mysql2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ const app = express();
 const PORT = 5001;
 const usersRoutes = require("./routes/users");
 const { logRequest, logResponse } = require("./middleware/logs");
+const mysql = require("mysql2");
+
+const dbPool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "express_rest",
+});
 
 // Middleware 1
 app.use(logRequest);
@@ -14,8 +22,6 @@ app.use(logResponse);
 app.use(express.json());
 
 app.use("/users", usersRoutes);
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running http://localhost:${PORT}`);
